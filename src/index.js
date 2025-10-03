4f4fa84f060dabcce9dcdfefdfaf2c17528b49d4
Jackob2004 <zjakub2004@gmail.com>
2025-10-03 15:59:24 +0200

    Implement selecting individual slides

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import "./styles.css";
 
 const carousel = document.querySelector("#carousel-container");
-const slides = Array.from(document.querySelector("#carousel-container").children);
+
+const slides = Array.from(
+  document.querySelector("#carousel-container").children,
+);
+
+const navigation = Array.from(document.querySelectorAll(".navigation"));
 
 let currentSlide = 0;
 
 document.querySelector("#btn-next").addEventListener("click", nextSlide);
 document.querySelector("#btn-prev").addEventListener("click", prevSlide);
+document
+  .querySelector("#direct-navigation")
+  .addEventListener("click", goToSlide);
 
 function nextSlide() {
+  const oldSlide = currentSlide;
   if (currentSlide + 1 > slides.length - 1) {
     currentSlide = 0;
   } else {
@@ -16,9 +25,11 @@ function nextSlide() {
   }
 
   carousel.replaceChildren(slides[currentSlide]);
+  markNavigation(oldSlide);
 }
 
 function prevSlide() {
+  const oldSlide = currentSlide;
   if (currentSlide - 1 < 0) {
     currentSlide = slides.length - 1;
   } else {
@@ -26,4 +37,29 @@ function prevSlide() {
   }
 
   carousel.replaceChildren(slides[currentSlide]);
+  markNavigation(oldSlide);
+}
+
+/**
+ *
+ * @param {Event} e
+ */
+function goToSlide(e) {
+  const index = Number(e.target.dataset.image);
+
+  if (!index && index !== 0) return;
+  const oldSlide = currentSlide;
+  currentSlide = index;
+
+  carousel.replaceChildren(slides[currentSlide]);
+  markNavigation(oldSlide);
+}
+
+/**
+ *
+ * @param {number} oldSlide
+ */
+function markNavigation(oldSlide) {
+  navigation[oldSlide].classList.remove("selected");
+  navigation[currentSlide].classList.add("selected");
 }
